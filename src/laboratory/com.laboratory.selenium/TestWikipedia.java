e9027d37f86198f5e0d88a7ab76c00b66b16a756
msavane <savanemory@outlook.com>
2022-02-13 11:21:06 -0500

    adding some jars to make selenium work w/ editor

--- src/laboratory/com.laboratory.selenium/TestWikipedia.java
+++ src/laboratory/com.laboratory.selenium/TestWikipedia.java
@@ -1,21 +1,27 @@
 package laboratory.com.laboratory.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class TestWikipedia {
 
     public static void main(String[]args) throws InterruptedException{
-        /*Webdriver driver = new ChromeDriver();
-        driver.get("http://wikipedia.org");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://wikipedia.com");
         WebElement link;
-        link = driver.findelement(By.linkText("English"));
-        link.click;
+        link = driver.findElement(By.id("js-link-box-en"));
+        link.click();
         Thread.sleep(5000);
         WebElement searchbox;
         searchbox = driver.findElement(By.id("searchInput"));
-        searchBox.sendKeys("software");
+        searchbox.sendKeys("software");
          Thread.sleep(5000);
-         driver.quit();
-        */
+         //driver.quit();
+
     }
 
 
